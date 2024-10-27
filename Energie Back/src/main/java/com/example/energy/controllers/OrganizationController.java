69004b34bfa2c7c5959199d4d299ad3cc1425a0b
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-10-27 19:56:55 +0100

    ..

---
+++ Energie Back/src/main/java/com/example/energy/controllers/OrganizationController.java
@@ -0,0 +1,2 @@
+package com.example.energy.controllers;public class OrganizationController {
+}
---
+++ Energie Back/src/main/java/com/example/energy/entities/OrganizationRequest.java
@@ -0,0 +1,14 @@
+package com.example.energy.entities;
+
+public class OrganizationRequest {
+    private String name;
+
+    // Getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
