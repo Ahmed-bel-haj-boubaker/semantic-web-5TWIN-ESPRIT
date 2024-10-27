266779576aa96ad6b79600e32801a488574ae487
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-10-27 21:16:02 +0100

    test

--- Energie Back/src/main/java/com/example/energy/entities/Organization.java
+++ Energie Back/src/main/java/com/example/energy/entities/Organization.java
@@ -1,22 +1,19 @@
 package com.example.energy.entities;
 
-
-
-
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-
-
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Organization {
-
-
       String nom;
+      OrganizationG organizationG;
+
+      public Organization(String nom) {
+            this.nom = nom;
+      }
 
 }
